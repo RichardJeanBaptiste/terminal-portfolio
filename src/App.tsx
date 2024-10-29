b55431c333db077823728c4c726ed38290e926a3
Richard Jean-Baptiste <Richinbk1@gmail.com>
2024-10-28 22:49:46 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='h-screen w-screen' style={{
+      backgroundColor: 'rgb(40, 42, 54)'
+    }}>
+      
+    </div>
+  )
+}
+
+export default App
