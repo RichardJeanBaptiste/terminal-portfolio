01c7dace46713591d1e037bd504b55ee6975b690
Richard Jean-Baptiste <Richinbk1@gmail.com>
2024-11-05 08:59:35 -0500

    added Date Object and some styling to <Prompt/>

--- src/components/prompt.tsx
+++ src/components/prompt.tsx
@@ -1,8 +1,23 @@
+import { useState, useEffect } from "react";
+
 
 function Prompt() {
+
+    const [currentDate, SetCurrentDate] = useState("");
+    const [currentDir, SetCurrentDir] = useState("");
+
+    useEffect(() => {
+        SetCurrentDate(new Date().toUTCString());
+        SetCurrentDir("user@RichardJ-Portfolio ~ %");
+    },[]);
     return (
-        <div>
-            <p>Terminal Prompt</p>
+        <div className="w-full h-full">
+            <p>Date: {currentDate}</p>
+            {/******************* Cursor Div *********/}
+            <div style={{ display: 'flex', flexDirection: 'row'}} className="w-full h-6">
+                <p>{currentDir}</p>
+                <div style={{ marginLeft: '.4%', marginTop: '.1%', width: '10px', height: '20px', borderColor: 'white', borderStyle: 'solid', borderWidth: '1px'}}/>
+            </div>
         </div>
     );
 }
